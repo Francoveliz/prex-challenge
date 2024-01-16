a48ca8d5a9a7354fb37e7409e2b66962d13e46e8
francoveliz <franco@robosophy.com>
2024-01-16 18:18:08 -0300

    fix: useLocalStorage ssr bug

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,39 +1,51 @@
 import { useState, useEffect } from "react";
 
-const useLocalStorage = (key: any, defaultValue: any) => {
+const useLocalStorage = (key: string, defaultValue: any) => {
+  const isClient = typeof window !== "undefined"; // Verifica si se está ejecutando en un entorno de navegador
+
   const [value, setValue] = useState(() => {
     try {
-      const storedValue = localStorage.getItem(key);
-      return storedValue ? JSON.parse(storedValue) : defaultValue;
+      // Solo intenta acceder a localStorage si se está ejecutando en el navegador
+      if (isClient) {
+        const storedValue = localStorage.getItem(key);
+        return storedValue ? JSON.parse(storedValue) : defaultValue;
+      }
     } catch (error) {
       console.error("Error parsing stored value:", error);
-      return defaultValue;
     }
+
+    return defaultValue;
   });
 
   useEffect(() => {
-    const handleStorageChange = (event: any) => {
-      if (event.key === key) {
-        try {
-          setValue(event.newValue ? JSON.parse(event.newValue) : null);
-        } catch (error) {
-          console.error("Error parsing changed value:", error);
+    // Solo suscríbete a cambios en localStorage si se está ejecutando en el navegador
+    if (isClient) {
+      const handleStorageChange = (event: any) => {
+        if (event.key === key) {
+          try {
+            setValue(event.newValue ? JSON.parse(event.newValue) : null);
+          } catch (error) {
+            console.error("Error parsing changed value:", error);
+          }
         }
-      }
-    };
+      };
 
-    window.addEventListener("storage", handleStorageChange);
+      window.addEventListener("storage", handleStorageChange);
 
-    return () => {
-      window.removeEventListener("storage", handleStorageChange);
-    };
-  }, [key]);
+      return () => {
+        window.removeEventListener("storage", handleStorageChange);
+      };
+    }
+  }, [key, isClient]);
 
   const updateValue = (newValue: any) => {
     try {
-      const newValueString = JSON.stringify(newValue);
-      localStorage.setItem(key, newValueString);
-      setValue(newValue);
+      // Solo actualiza localStorage si se está ejecutando en el navegador
+      if (isClient) {
+        const newValueString = JSON.stringify(newValue);
+        localStorage.setItem(key, newValueString);
+        setValue(newValue);
+      }
     } catch (error) {
       console.error("Error updating local storage:", error);
     }
